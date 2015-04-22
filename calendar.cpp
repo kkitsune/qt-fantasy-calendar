9ffe974fdd298aedad3fc2d282c616c9a828bae1
Daniel Grondin <tm.kkitsune@gmail.com>
2015-04-21 22:37:41 -0400

    Added database's table creation (schema subject to change)

--- calendar.cpp
+++ calendar.cpp
@@ -1,4 +1,7 @@
 #include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
 #include <QDir>
 
 #include "calendar.h"
@@ -11,6 +14,19 @@ Calendar::Calendar(const QString& name, QObject* parent)
 	auto db = QSqlDatabase::addDatabase("QSQLITE");
 	db.setDatabaseName(QDir().absolutePath() + Directory + name + ".db");
 	db.open();
+
+	if(db.tables().empty())
+	{
+		QSqlQuery q;
+		if(!q.exec("CREATE TABLE 'Moon' ('id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 'Name' TEXT NOT NULL UNIQUE, 'Revolution' INTEGER NOT NULL)"))
+			qFatal("SQL Error : %s", q.lastError().databaseText().toStdString().c_str());
+		if(!q.exec("CREATE TABLE 'Month' ('id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 'Name' TEXT NOT NULL UNIQUE, 'Length' INTEGER NOT NULL)"))
+			qFatal("SQL Error : %s", q.lastError().databaseText().toStdString().c_str());
+		if(!q.exec("CREATE TABLE 'Day' ('id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 'Name' TEXT NOT NULL UNIQUE)"))
+			qFatal("SQL Error : %s", q.lastError().databaseText().toStdString().c_str());
+		if(!q.exec("CREATE TABLE 'Event' ('id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, 'Month' INTEGER NOT NULL, 'Day' INTEGER NOT NULL, 'Year' INTEGER NOT NULL CHECK(Year > 0), 'Name' TEXT NOT NULL UNIQUE, 'Description' TEXT, FOREIGN KEY('Month') REFERENCES 'Month', FOREIGN KEY('Day') REFERENCES 'Day')"))
+			qFatal("SQL Error : %s", q.lastError().databaseText().toStdString().c_str());
+	}
 }
 
 Calendar::~Calendar()
